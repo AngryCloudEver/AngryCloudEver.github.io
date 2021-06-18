87a82ffd1365b953733b7d20f70945c2886c2888
AngryCloudEver <68358555+AngryCloudEver@users.noreply.github.com>
2021-06-18 14:56:54 +0700

    Update Pricing Responsive

--- My Creation/Creation 1/js/home.js
+++ My Creation/Creation 1/js/home.js
@@ -1,4 +1,5 @@
+let productImageWidth = $("#productLeftImage").children().css('width');
+$("#productLeftImage").children().css('height', productImageWidth);
+
 $(window).on('load', function(event){
-    let productImageWidth = $("#productLeftImage").children().css('width');
-    $("#productLeftImage").children().css('height', productImageWidth);
 });
